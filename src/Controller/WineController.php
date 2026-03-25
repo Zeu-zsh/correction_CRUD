c5bddf4414be51057de4a4a0854b75a92f51522c
Your Name <you@example.com>
2026-03-25 15:06:51 +0100

    Add Wine entity, controller, form, and templates for CRUD operations

---
+++ src/Controller/WineController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Wine;
+use App\Form\WineType;
+use App\Repository\WineRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class WineController extends AbstractController
+{
+    #[Route('/wine', name: 'app_wine_index')]
+    public function index(WineRepository $wineRepository): Response
+    {
+        return $this->render('wine/index.html.twig', [
+            'wines' => $wineRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/wine/new', name: 'app_wine_create')]
+    public function create(Request $request, EntityManagerInterface $em): Response
+    {
+        $wine = new Wine();
+        $form = $this->createForm(WineType::class, $wine);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            // dump($wine);
+            $em->persist($wine);
+            $em->flush();
+            // dump($wine);
+        }
+        
+        return $this->render('wine/new.html.twig', [
+            'wine_form' => $form,
+        ]);
+    }
+
+    #[Route('/wine/edit', name: 'app_wine_edit')]
+    public function edit(): Response
+    {
+        return $this->render('wine/edit.html.twig', [
+            'controller_name' => 'WineController',
+        ]);
+    }
+
+    #[Route('/wine/delete', name: 'app_wine_delete')]
+    public function delete(): Response
+    {
+        return $this->redirectToRoute("app_wine_index");
+    }
+}
